1ede64db0a4e722e814b070a52078bd87f4a3545
hucy <hucy@gegolab.com>
2018-04-23 08:57:54 +0800

    add onload

--- lesson1/scripts/showPic.js
+++ lesson1/scripts/showPic.js
@@ -1,8 +1,79 @@
 
-function showPic(whichpic) {
-    var source = whichpic.getAttribute("href");
-    var placeholder = document.getElementById("placeholder");
+function prepareGallery() {
+    if ( !document.getElementById) {
+        return false;
+    }
+    if ( !document.getElementsByTagName) {
+        return false;
+    }
+    if ( !document.getElementById("imageGallery")) {
+        return false;
+    }
 
-    placeholder.setAttribute("src", source);
+    var gallery = document.getElementById("imageGallery");
+    var links = gallery.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function () {
+            return showPic(this);
+        }
+    }
 }
 
+function showPic(whichpic) {
+    if ( !document.getElementById("placeholder")) {
+        return true;
+    }
+    var source = whichpic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    if (placeholder.nodeName != "IMG") {
+        return true;
+    }
+
+    placeholder.setAttribute("src", source);
+    if ( !document.getElementById("description")) {
+        return false;
+    }
+    var title = '';
+    if ( whichpic.getAttribute("title")) {
+        title = whichpic.getAttribute("title");
+    }
+    var description = document.getElementById("description");
+    if (description.firstChild.nodeType == 3) {
+        description.firstChild.nodeValue = title;
+    }
+    return false;
+}
+
+// window.onload = prepareGallery;
+
+addOldEvent(prepareGallery);
+addOldEvent(prepareLink);
+
+function addOldEvent(fun) {
+    var oldonLoad = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = fun;
+    } else {
+        window.onload = function () {
+            oldonLoad();
+            fun();
+        }
+    }
+}
+
+
+function popUp(windUrl) {
+    window.open(windUrl, "popUp", "weight=300, height=200");
+}
+
+function prepareLink() {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].getAttribute("class") == "popup") {
+            links[i].onclick = function () {
+                popUp(this.getAttribute("href"));
+                return false;
+            }
+        }
+    }
+}
